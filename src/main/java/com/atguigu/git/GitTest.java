0e679e863ba1eb738f4939e53ced64dfb719c02d
HappyDog <985114605@qq.com>
2025-10-22 23:36:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Dear_Monster
+ * @create 2025-10-22 23:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
